c355c03bc694d7bdf357b6f393641ab64d59c4d9
송찬의 <dforce103@gmail.com>
2025-12-05 15:31:48 +0900

    RsData 제너릭 타입으로 수정

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -3,8 +3,10 @@ package com.back.domain.post.post.controller;
 import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +19,7 @@ public class ApiV1PostController {
 
     @GetMapping
 //    @ResponseBody // 이 어노테이션이 있어야 JSON으로 응답한다. -> @Controller -> @RestController
+    @Transactional(readOnly = true)
     public List<PostDto> getItems() {
         List<Post> items = postService.findAll();
 
@@ -27,10 +30,25 @@ public class ApiV1PostController {
     }
 
     @GetMapping("/{id}")
+    @Transactional(readOnly = true)
     public PostDto getItem(@PathVariable int id) {
         Post post = postService.findById(id).get();
 
         return new PostDto(post);
+    }
+
+    @GetMapping("/{id}/delete")
+    @Transactional
+    public RsData<PostDto> delete(@PathVariable int id) {
+        Post post = postService.findById(id).get();
+
+        postService.delete(post);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 글이 삭제되었습니다.".formatted(id),
+                new PostDto(post)
+        );
     }
 
 }
--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -1,11 +1,12 @@
 package com.back.domain.post.postComment.controller;
 
-import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 @RequestMapping("/api/v1/posts/{postId}/comments")
 @RequiredArgsConstructor
-@RestController
 public class ApiV1PostCommentController {
     private final PostService postService;
 
     @GetMapping
+    @Transactional(readOnly = true)
     public List<PostCommentDto> getItems(
             @PathVariable int postId
     ) {
@@ -33,6 +35,7 @@ public class ApiV1PostCommentController {
     }
 
     @GetMapping("/{id}")
+    @Transactional(readOnly = true)
     public PostCommentDto getItem(
             @PathVariable int postId,
             @PathVariable int id
@@ -45,7 +48,8 @@ public class ApiV1PostCommentController {
     }
 
     @GetMapping("/{id}/delete")
-    public PostCommentDto deleteItem(
+    @Transactional
+    public RsData<PostCommentDto> delete(
             @PathVariable int postId,
             @PathVariable int id
     ) {
@@ -55,7 +59,10 @@ public class ApiV1PostCommentController {
 
         postService.deleteComment(post, postComment);
 
-        return new PostCommentDto(postComment);
+        return new RsData<>(
+                "200-1",
+                "%d번 댓글이 삭제되었습니다.".formatted(id),
+                new PostCommentDto(postComment)
+        );
     }
-
 }

448ad27c540f8f042c7d951838e9219ecef9d697
송찬의 <dforce103@gmail.com>
2025-12-05 11:08:13 +0900

    @RestController로 변경

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -5,18 +5,20 @@ import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class ApiV1PostController {
     private final PostService postService;
 
-    @GetMapping("/api/v1/posts")
-    @ResponseBody // 이 어노테이션이 있어야 JSON으로 응답한다.
+    @GetMapping("/posts")
+//    @ResponseBody // 이 어노테이션이 있어야 JSON으로 응답한다. -> @Controller -> @RestController
     public List<Post> getItems() {
         List<Post> items = postService.findAll();
 
